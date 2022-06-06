e2d1723ee50d54d0fb3188e08ab1a1d3be6dac0b
若海 <wang@rehiy.com>
2022-06-06 08:57:02 +0800

    添加注释

--- api/user/router.go
+++ api/user/router.go
@@ -8,12 +8,16 @@ import (
 
 func Router(api *gin.RouterGroup) {
 
+	// 匿名接口
+
 	rg1 := api.Group("/user")
 
 	{
 		rg1.POST("/login", login)
 		rg1.POST("/register", register)
 	}
+
+	// 需授权接口
 
 	rg2 := api.Group("/user")
 
