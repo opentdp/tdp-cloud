9ba02e27f027ae1b38240deae2139a34442c5ab1
若海 <wang@rehiy.com>
2022-06-16 06:22:16 +0800

    修改帮助

--- core/cli/flag.go
+++ core/cli/flag.go
@@ -14,21 +14,29 @@ var (
 func Flags() {
 
 	flag.StringVar(&Dsn, "dsn", "cloud.db", "数据库存储路径")
-
-	flag.StringVar(&Address, "address", ":7800", "服务器监听地址和端口")
+	flag.StringVar(&Address, "address", ":7800", "服务监听地址和端口")
 
 	flag.Usage = usage
 
 	flag.Parse()
+
 }
 
 func usage() {
 
-	fmt.Fprintf(os.Stderr, `轻量服务器控制面板，项目地址 https://github.com/tdp-resource/tdp-cloud
+	fmt.Fprintf(os.Stdout, `
+轻量服务器控制面板
 
 可选参数:
 
 `)
 
 	flag.PrintDefaults()
+
+	fmt.Fprintf(os.Stdout, `
+开源项目 https://github.com/tdp-resource/tdp-cloud
+问题提交 https://github.com/tdp-resource/tdp-cloud/issues
+
+`)
+
 }
