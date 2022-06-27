bbb061c63e99fc2f8e21012d53f0cbbf20bdb099
若海 <wang@rehiy.com>
2022-06-27 11:09:13 +0800

    捕获数据异常

--- core/midware/output.go
+++ core/midware/output.go
@@ -11,16 +11,19 @@ func JSON() gin.HandlerFunc {
 		c.Next()
 
 		// 输出错误信息
-		if err, exists := c.Get("Error"); exists {
+		if err, exists := c.Get("Error"); exists && err != nil {
 			c.AbortWithStatusJSON(400, NewError(err))
 			return
 		}
 
 		// 输出请求结果
-		if res, exists := c.Get("Payload"); exists {
+		if res, exists := c.Get("Payload"); exists && res != nil {
 			c.AbortWithStatusJSON(200, NewPayload(res))
 			return
 		}
+
+		// 捕获异常返回
+		c.AbortWithStatusJSON(500, NewError("内部错误"))
 
 	}
 
