bf0a06ca50ada57f32d1f71c61fe614d34fffddd
若海 <wang@rehiy.com>
2022-08-10 20:08:20 +0800

    支持重新连接

--- core/slave/main.go
+++ core/slave/main.go
@@ -13,8 +13,21 @@ func Connect(url string) {
 
 	ws, _, err := websocket.DefaultDialer.Dial(url, nil)
 	if err != nil {
-		log.Fatal(err)
+		log.Println("无法连接，将在5秒后重试")
+		time.Sleep(time.Second * 5)
+		Connect(url)
+		return
 	}
+
+	// 断线重连
+
+	defer func() {
+		log.Println("连接断开，将在5秒后重试")
+		time.Sleep(time.Second * 5)
+		Connect(url)
+	}()
+
+	// 保持连接
 
 	go func() {
 		var v = SocketData{
@@ -25,16 +38,20 @@ func Connect(url string) {
 		for {
 			log.Println("send: ", v)
 			if err := ws.WriteJSON(v); err != nil {
-				log.Fatal(err)
+				log.Println(err)
+				break
 			}
 			time.Sleep(time.Second * 30)
 		}
 	}()
 
+	// 接收数据
+
 	for {
 		var v = SocketData{}
 		if err := ws.ReadJSON(&v); err != nil {
-			log.Fatal(err)
+			log.Println(err)
+			break
 		}
 		log.Println("receive: ", v)
 	}
