20df2392b6e74957a87a81cd5bd5e25613f42304
若海 <wang@rehiy.com>
2023-01-19 19:43:03 +0800

    增加默认参数

--- serve.bat
+++ serve.bat
@@ -11,6 +11,6 @@ SET GO111MODULE=on
 CD /d %~dp0
 
 CALL go mod tidy
-CALL go run main.go server --listen 127.0.0.1:7800
+CALL go run main.go server --listen 127.0.0.1:7800 --dsn cloud.db?_pragma=busy_timeout=5000&_pragma=journa_mode(WAL)
 
 IF "%1" == "" CMD /K
