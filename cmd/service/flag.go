c4b25883d009cd1c728c9c941f170e72c7af69c6
若海 <wang@rehiy.com>
2023-01-21 09:05:39 +0800

    优化帮助输出

--- cmd/service/flag.go
+++ cmd/service/flag.go
@@ -18,8 +18,8 @@ func Flags() *cmd.FlagSet {
 		Comment: "服务管理",
 	}
 
-	cmd.StringVar(&vInstall, "install", "", "安装服务 [server|worker]")
-	cmd.StringVar(&vUninstall, "uninstall", "", "卸载服务 [server|worker]")
+	cmd.StringVar(&vInstall, "install", "", "安装服务 [ server | worker ]")
+	cmd.StringVar(&vUninstall, "uninstall", "", "卸载服务 [ server | worker ]")
 
 	return cmd
 
