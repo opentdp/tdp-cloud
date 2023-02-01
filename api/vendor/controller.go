962111fcffad4f7f643bee765efe833e3cbc3702
若海 <wang@rehiy.com>
2023-02-01 12:37:43 +0800

    新增获取单个厂商的接口

--- api/vendor/controller.go
+++ api/vendor/controller.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/cast"
 
+	"tdp-cloud/module/dborm"
 	"tdp-cloud/module/dborm/vendor"
 )
 
@@ -16,6 +17,23 @@ func list(c *gin.Context) {
 	if lst, err := vendor.FetchAll(userId); err == nil {
 		vendor.SecretMask(lst)
 		c.Set("Payload", gin.H{"Datasets": lst})
+	} else {
+		c.Set("Error", err)
+	}
+
+}
+
+// 获取厂商
+
+func detail(c *gin.Context) {
+
+	userId := c.GetUint("UserId")
+	id := cast.ToUint(c.Param("id"))
+
+	if res, err := vendor.Fetch(id, userId); err == nil {
+		lst := []*dborm.Vendor{res}
+		vendor.SecretMask(lst)
+		c.Set("Payload", res)
 	} else {
 		c.Set("Error", err)
 	}
--- api/vendor/router.go
+++ api/vendor/router.go
@@ -15,6 +15,7 @@ func Router(api *gin.RouterGroup) {
 	{
 		rg.GET("/vendor", list)
 		rg.POST("/vendor", create)
+		rg.GET("/vendor/:id", detail)
 		rg.PATCH("/vendor/:id", update)
 		rg.DELETE("/vendor/:id", delete)
 	}
