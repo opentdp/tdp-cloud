4c97bc828cec03e99a1aa396e6dfa9bf5bfa19d8
若海 <wang@rehiy.com>
2023-02-03 00:42:34 +0800

    chore: 调整安装upx兼容性

--- build.sh
+++ build.sh
@@ -6,13 +6,8 @@ export GO111MODULE=on
 
 build() {
     echo building for $1/$2
-
     out=build/tdp-cloud-$1-$2$3
     GOOS=$1 GOARCH=$2 go build -ldflags="-s -w" -o $out main.go
-
-    if type upx >/dev/null 2>&1; then
-        upx --best $out
-    fi
 }
 
 ####################################################################
@@ -27,3 +22,19 @@ build linux arm64
 
 build windows amd64 .exe
 build windows arm64 .exe
+
+####################################################################
+
+if type apt >/dev/null 2>&1; then
+    sudo apt install -y upx-ucl
+fi
+
+if type apk >/dev/null 2>&1; then
+    apk add upx
+fi
+
+if type upx >/dev/null 2>&1; then
+    cd build
+    upx --best `ls .`
+    cd ..
+fi
