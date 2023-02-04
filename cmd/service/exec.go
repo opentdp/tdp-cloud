13b086cb3d3ff0cd7f5869b11b998def71864903
若海 <wang@rehiy.com>
2023-02-04 21:03:31 +0800

    refactor: 模块 installer 更改为 service

--- cmd/service/exec.go
+++ cmd/service/exec.go
@@ -1,16 +1,57 @@
 package service
 
 import (
+	"log"
+
 	"github.com/spf13/viper"
 
-	"tdp-cloud/module/installer"
+	"tdp-cloud/module/service"
 )
 
 func Execute() {
 
-	in := viper.GetString("install")
-	un := viper.GetString("uninstall")
+	if in := viper.GetString("install"); in != "" {
+		logPrint("uninstall", uninstall(in))
+	}
 
-	installer.Execute(in, un)
+	if un := viper.GetString("uninstall"); un != "" {
+		logPrint("install", install(un))
+	}
+
+}
+
+func logPrint(n string, e error) {
+
+	if e != nil {
+		log.Fatalln(n, "service error:", e.Error())
+	} else {
+		log.Println(n, "service done")
+	}
+
+}
+
+func install(name string) error {
+
+	switch name {
+	case "server":
+		return service.Server().Install()
+	case "worker":
+		return service.Worker().Install()
+	}
+
+	return nil
+
+}
+
+func uninstall(name string) error {
+
+	switch name {
+	case "server":
+		return service.Server().Uninstall()
+	case "worker":
+		return service.Worker().Uninstall()
+	}
+
+	return nil
 
 }
