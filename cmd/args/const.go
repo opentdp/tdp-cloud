543b72e26d7bf55d319197190dd9386400abf4b5
若海 <wang@rehiy.com>
2023-02-17 12:36:45 +0800

    style: 说明内容放到单独的文件

--- cmd/args/const.go
+++ cmd/args/const.go
@@ -10,8 +10,3 @@ var ConfigFile string
 
 var Version = "0.5.1"
 var BuildVersion = "202301"
-
-var ReadmeText = `土豆片控制面板
-
-开源项目 https://github.com/open-tdp/tdp-cloud
-问题提交 https://github.com/open-tdp/tdp-cloud/issues`
---
+++ cmd/args/readme.go
@@ -0,0 +1,6 @@
+package args
+
+var ReadmeText = `土豆片控制面板
+
+开源项目 https://github.com/open-tdp/tdp-cloud
+问题提交 https://github.com/open-tdp/tdp-cloud/issues`
