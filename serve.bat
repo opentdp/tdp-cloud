62cdc1914cf2439cc6eeb792b56b0ca2b96c0f5e
若海 <wang@rehiy.com>
2023-02-20 22:02:07 +0800

    chore: 调整开发服务配置

--- serve.bat
+++ serve.bat
@@ -10,7 +10,5 @@ SET GO111MODULE=on
 
 CD /d %~dp0
 
-CALL go mod tidy
-CALL go run main.go server --listen 127.0.0.1:7800
-
-IF "%1" == "" CMD /K
+go mod tidy
+go run main.go server --listen 127.0.0.1:7800
