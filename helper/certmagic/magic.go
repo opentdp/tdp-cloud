7bb5096cf5542e251511094f9b02bcc29a1a49e3
若海 <wang@rehiy.com>
2023-03-03 21:55:14 +0800

    fix: 无法获取旧证书

--- helper/certmagic/magic.go
+++ helper/certmagic/magic.go
@@ -2,6 +2,7 @@ package certmagic
 
 import (
 	"context"
+	"strings"
 
 	"github.com/caddyserver/certmagic"
 	"go.uber.org/zap"
@@ -27,10 +28,16 @@ func newMagic(iss certmagic.ACMEIssuer) *certmagic.Config {
 
 	evtlog := logman.Named("cert.event")
 	config.OnEvent = func(ctx context.Context, evt string, data map[string]any) error {
-		if CertEvent != nil && data["identifier"] != nil {
-			CertEvent(evt, data)
-		}
 		evtlog.Warn(evt, zap.Any("data", data))
+		if CertEvent != nil {
+			switch evt {
+			case "cert_obtaining", "cert_failed", "cert_obtained":
+				CertEvent(strings.Split(evt, "_")[1], data)
+			case "cached_managed_cert", "cached_unmanaged_cert":
+				data["identifier"] = strings.Join(data["sans"].([]string), ",")
+				CertEvent("cached", data)
+			}
+		}
 		return nil
 	}
 
