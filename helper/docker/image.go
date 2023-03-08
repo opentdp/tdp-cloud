85f845856854bf09863a17d1be5764349418d1d2
若海 <wang@rehiy.com>
2023-03-08 14:14:01 +0800

    feat: 新增docker通用接口库

--- helper/docker/image.go
+++ helper/docker/image.go
@@ -7,6 +7,9 @@ import (
 	"io"
 
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/filters"
+	"github.com/docker/docker/api/types/image"
+	"github.com/docker/docker/api/types/registry"
 )
 
 // 拉取镜像
@@ -31,5 +34,82 @@ func (dc *DockerClient) ImagePull(rq *ImagePullParam) (io.ReadCloser, error) {
 	}
 
 	return dc.Client.ImagePull(ctx, rq.ImageName, option)
+
+}
+
+// 列出镜像
+
+func (dc *DockerClient) ImageList() ([]types.ImageSummary, error) {
+
+	ctx := context.Background()
+
+	images, err := dc.Client.ImageList(ctx, types.ImageListOptions{})
+	return images, err
+
+}
+
+// 查找镜像
+
+func (dc *DockerClient) ImageSearch(imageName string) ([]registry.SearchResult, error) {
+
+	ctx := context.Background()
+
+	images, err := dc.Client.ImageSearch(ctx, imageName, types.ImageSearchOptions{})
+	return images, err
+
+}
+
+// 获取镜像信息
+
+func (dc *DockerClient) ImageInspect(imageName string) (types.ImageInspect, error) {
+
+	ctx := context.Background()
+
+	image, _, err := dc.Client.ImageInspectWithRaw(ctx, imageName)
+	return image, err
+
+}
+
+// 获取镜像历史
+
+func (dc *DockerClient) ImageHistory(imageName string) ([]image.HistoryResponseItem, error) {
+
+	ctx := context.Background()
+
+	history, err := dc.Client.ImageHistory(ctx, imageName)
+	return history, err
+
+}
+
+// 获取镜像标签
+
+func (dc *DockerClient) ImageTag(imageName, tag string) error {
+
+	ctx := context.Background()
+
+	err := dc.Client.ImageTag(ctx, imageName, tag)
+	return err
+
+}
+
+// 删除镜像
+
+func (dc *DockerClient) ImageRemove(imageName string) error {
+
+	ctx := context.Background()
+
+	_, err := dc.Client.ImageRemove(ctx, imageName, types.ImageRemoveOptions{})
+	return err
+
+}
+
+// 清理镜像
+
+func (dc *DockerClient) ImagePrune() error {
+
+	ctx := context.Background()
+
+	_, err := dc.Client.ImagesPrune(ctx, filters.Args{})
+	return err
 
 }
