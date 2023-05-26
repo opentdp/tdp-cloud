d686d092a6bc73ad27fae49349214e03fa94771e
若海 <wang@rehiy.com>
2023-05-26 12:31:32 +0800

    release v0.14.0

--- cmd/args/build.go
+++ cmd/args/build.go
@@ -1,7 +1,7 @@
 package args
 
-const Version = "0.13.3"
-const BuildVersion = "230501"
+const Version = "0.14.0"
+const BuildVersion = "230502"
 
 const AppName = "TDP CLoud"
 const AppSummary = "土豆片云面板"
